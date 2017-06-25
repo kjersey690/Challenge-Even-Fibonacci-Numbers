8ddb7791fbf4282bdc2380b568916a7557f46388
Keith Johnson <kjersey690@gmail.com>
2017-06-25 01:13:59 -0400

    using pseudo code to think through the solution

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 /**
  * Return the total sum of all even fibonacci numbers up to and including
  * the value given to you at `maxFibValue`
@@ -6,24 +7,43 @@
  * @return {Number} sum
  */
 function _sumFibs( maxFibValue ) {
-  var sum = 0;
+  var a= 1;
+   var b=0;
+   let temp;
+   let arr=[];
 
-  // do your work here
+ do{
+   temp = a;
+      a = a+b;
+      b = temp;
+    if(temp%2 ===0){
+     arr.push(temp);
+    }
+
+ }
+ while(temp <= maxFibValue);
+
+  let sum=arr.reduce((prev,current)=>{
+    return prev+current;
+  },0);
 
   return sum;
 }
 
-// bonus round
+
+
+
+// // bonus round
 function _highestFibonacciNumber (maxFibValue){
-  var highest = 0;
+//   var highest = 0;
 
-  //define your base case, validate your input
+//   //define your base case, validate your input
 
 
-  //do your work here
+//   //do your work here
 
-  return highest;
-};
+//   return highest;
+ };
 
 /**
  * Do not modify code below.
